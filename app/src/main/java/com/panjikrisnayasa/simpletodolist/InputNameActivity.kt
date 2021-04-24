7b6f56f6d0d75a75e38eaefe22a6097d375c337b
I Putu Panji Krisnayasa <48062932+panjikrisnayasa@users.noreply.github.com>
2021-04-24 12:08:42 +0800

    Initial commit with input name activity layout done

---
+++ app/src/main/java/com/panjikrisnayasa/simpletodolist/InputNameActivity.kt
@@ -0,0 +1,11 @@
+package com.panjikrisnayasa.simpletodolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_name)
+    }
+}
